aad67d17dbe2b6a83cf6e95e690d17895d634aad
Stanislav Roll <Stanislav.Roll@student.souepl.cz>
2025-12-01 13:46:12 +0100

    Commmit to the bazaar

---
+++ ui/car_table.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import ttk
+
+class CarTable(ttk.Frame):
+    COLUMNS = ("id", "brand", "model", "year", "price", "colour", "mileage")
+    COLUMNS_NAME = {
+        "id": "ID",
+        "brand": "Značka",
+        "year": "Rok",
+        "price": "Cena (Kč)",
+        "colour": "Barva",
+        "mileage": "Najeto (km)"
+    }
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._create_widgets()
+        
+    def _create_widgets(self):
+        """Vytvoří tabulku a scrollbar"""
+        style = ttk.Style()
+
+        style.configure("Treeview", rowheight=30, font=("Seguo UI", 10)) # řádka v tabulce
+        style.configure("Treeview.Heading", font=("Segue UI", 10, "bold")) # záhlaví #tupple
+
+        # Vytváření tabulky
+        self.tree = ttk.Treeview(
+            self,
+            columns=self.COLUMNS,
+            show="headings"
+            selectmode="browse"
+        )
+
+        #Scrollbar
+        self.scrollbar = ttk.Scrollbar(
+            self,
+            orient=tk.VERTICAL,
+            command=self.tree.yview
+        )
+
+        self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
