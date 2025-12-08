c5dc400f2d3ddb1c5977bc9f2b8db6c652c8cdd6
Stanislav Roll <Stanislav.Roll@student.souepl.cz>
2025-12-08 08:15:56 +0100

    A ulož se

--- ui/car_table.py
+++ ui/car_table.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 class CarTable(ttk.Frame):
     COLUMNS = ("id", "brand", "model", "year", "price", "colour", "mileage")
-    COLUMNS_NAME = {
+    COLUMNS_NAME = { #Python - klíč/key
         "id": "ID",
         "brand": "Značka",
         "year": "Rok",
@@ -19,7 +19,6 @@ class CarTable(ttk.Frame):
     def _create_widgets(self):
         """Vytvoří tabulku a scrollbar"""
         style = ttk.Style()
-
         style.configure("Treeview", rowheight=30, font=("Seguo UI", 10)) # řádka v tabulce
         style.configure("Treeview.Heading", font=("Segue UI", 10, "bold")) # záhlaví #tupple
 
@@ -27,9 +26,10 @@ class CarTable(ttk.Frame):
         self.tree = ttk.Treeview(
             self,
             columns=self.COLUMNS,
-            show="headings"
+            show="headings",
             selectmode="browse"
-        )
+            )
+        
 
         #Scrollbar
         self.scrollbar = ttk.Scrollbar(
@@ -40,3 +40,16 @@ class CarTable(ttk.Frame):
 
         self.tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+
+    def _configure_columns(self):
+        """Nastavení hlavičky sloupců a jejich šířky"""
+        column_width = {
+            "id": 50, "brand": 100, "model":100, 
+            "year": 70, "price": 100, "colour": 100, 
+            "mileage": 100
+        }
+
+        for col in self.COLUMNS:
+            self.tree.heading(col, text=self.COLUMNS_NAME[col])
+            self.tree.column(col, width=column_width[col], anchor=tk.CENTER)
